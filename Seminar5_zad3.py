bf83b794104ef2532cf555266b6243ba77f8e2e3
Eliseev Denis <medved-ed@mail.ru>
2023-02-02 23:30:43 +0800

    zad1 zad2 zad3.v2

--- Seminar5_zad3.py
+++ Seminar5_zad3.py
@@ -1,51 +1,36 @@
-# Создайте программу для игры в ""Крестики-нолики""
+def run (): # Ход игрока
+    print (f'Ход игрока - {g[9]} введите номер ячейки для хода \nХод игрока 1 - Х, игрока 2 - О')
+    y = int (input('Ваш ход -'))
+    if ((g[y-1]!='X') or (g[y-1]!='O')) == True: # Проверка на занятость клетки
+        match g[9]: # Проверка очередности и ввод значения
+                case 1: 
+                        g[y-1] = 'X'
+                        g[9], g[10] = g[10], g[9]
+                case 2:
+                        g[y-1] = 'O'
+                        g[9], g[10] = g[10], g[9]
+    else:
+        run (g) # если клетка занята функция повторятется
 
-# Создать окно с кнопками
+def winner (g): # определение победителя
+        f = lambda g, x, y, z: g[x]==g[y]==g[z]
+        v = (bool (f(g,0,1,2)) or bool (f(g,3,4,5)) or bool (f(g,6,7,8)) 
+             or bool (f(g,2,4,6))  or bool (f(g,0,4,8))  or bool (f(g,0,3,6))  
+             or bool (f(g,1,4,7)) or bool (f(g,2,5,8)))
+        return v
+def game ():
+        i=0
+        while i<9:
+                if winner(g) == True:
+                        print (f'Выиграл игрок {g[10]}')
+                        break
+                else:
+                        run()
+                        gp(g)
+                        i+=1
 
-from tkinter import *
-from tkinter import ttk
-
-def clicked ():    
-        # b = Button(window, text=" X ") 
-        btn['text'] = 'X' 
-        
-window = Tk() #Создание окна
-gamer = 1
-window.title("Крестики - нолики") # создание наименования окна
-window.geometry ('200x200')
-text1 =  Label (window, text=f'Ход игрока {gamer}') # создание текста в окне для информирования игроков
-text1.grid (column=0, row=0)
-'''
-for i in range(1,4): # Создание кнопок
-    for j in range(3):
-        btn = Button(window, text="   ")  
-        #btn = Button(window, text="   ", command=clicked (gamer))  
-        btn.grid(column=j, row=i)
-'''
-btn1 = ttk.Button(window, text="   ")
-btn1.grid(column=0, row=1)
-btn2 = ttk.Button(window, text="   ")
-btn2.grid(column=1, row=1)
-btn3 = ttk.Button(window, text="   ")
-btn3.grid(column=2, row=1)
-btn4 = ttk.Button(window, text="   ")
-btn4.grid(column=0, row=2)
-btn5 = ttk.Button(window, text="   ")
-btn5.grid(column=1, row=2)
-btn6 = ttk.Button(window, text="   ")
-btn6.grid(column=2, row=2)
-btn7 = ttk.Button(window, text="   ")
-btn7.grid(column=0, row=3)
-btn8 = ttk.Button(window, text="   ")
-btn8.grid(column=1, row=3)
-btn9 = ttk.Button(window, text="   ")
-btn9.grid(column=2, row=3)
-print (type (btn1))
-window.bind_class ('Button','<ButtonPress-1>', clicked)
-window.mainloop() # не даёт закрыть окно
-
-
-
-
-# прописать логику при нажатии на кнопку в определённый ход
-# прописать условия выхода
+print ('Крестики-нолики, вариант в терминале')
+g = [1,2,3,4,5,6,7,8,9,1,2]
+gp = lambda g: print (f'|{g[0]}|{g[1]}|{g[2]}|\n|{g[3]}|{g[4]}|{g[5]}|\n|{g[6]}|{g[7]}|{g[8]}|')
+gp (g)
+game ()
